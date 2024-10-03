78de056d565ba27ebea7ea0ea965429e38810462
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-10-03 20:48:29 +0900

    test

--- src/main/java/org/cookieandkakao/babting/domain/calendar/dto/request/EventDetailGetResponseDto.java
+++
@@ -1,9 +0,0 @@
-package org.cookieandkakao.babting.domain.calendar.dto.request;
-
-import org.cookieandkakao.babting.domain.calendar.dto.response.EventGetResponseDto;
-
-public record EventDetailGetResponseDto(
-    EventGetResponseDto event
-) {
-
-}
